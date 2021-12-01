37039626690ff419ed23b4fa0967fa973006a8b6
sasunaru228 <od26102000@gmail.com>
2021-12-01 22:13:25 +0300

    styled Edit Count Button

--- src/Main/Product/EditButton/EditButton.js
+++ src/Main/Product/EditButton/EditButton.js
@@ -1,18 +1,26 @@
 import classes from "./EditButton.module.css";
+import like from "../like.svg"
+import minus from "./minus.svg"
+import plus from "./plus.svg"
 
 export default function EditButton(props){
     return(
         <span className={classes.holder}>
-            <span>
-                корзина
+            <span className={classes.first}>
+                <span>В корзине</span>
+                <span>Перейти</span>
             </span>
-            <span>
-                <span onClick={props.handleChangeMinus}>    -      </span>
-                {props.count}
-                <span onClick={props.handleChangePlus}>     +      </span>
+            <span className={classes.main}>
+                <span className={classes.sign} onClick={props.handleChangeMinus}>
+                    <img src={minus} alt="minus"/>
+                </span>
+                <span className={classes.count}>{props.count}</span>
+                <span className={classes.sign} onClick={props.handleChangePlus}>
+                    <img src={plus} alt="plus"/>
+                </span>
             </span>
-            <span>
-                like
+            <span className={classes.last}>
+                <img src={like} alt="like"/>
             </span>
         </span>
     )
